c49f269660b64e77678664e5bc9df4687903e95d
Mohammad <mo@mohammad.dev>
2022-05-06 23:23:10 -0600

    Initial Commit

---
+++ KeyCatcher/KeyCatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyCatcherApp.swift
+//  KeyCatcher
+//
+//  Created by Mohammad Al-Ahdal on 2022-05-06.
+//
+
+import SwiftUI
+
+@main
+struct KeyCatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
